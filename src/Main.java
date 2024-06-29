666786ef06abd5e834932649eaf8ea3d3ecce43d
Branyer <branyervergarac@gmail.com>
2024-06-29 02:15:38 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java_cup.runtime.Symbol;
+import ve.edu.unet.Lexico;
+
+import java.io.*;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)  {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+
+    }
+}
